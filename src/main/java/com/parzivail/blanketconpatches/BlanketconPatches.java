afe76608e40e7621aa375ecf504ba1108a552be4
Patbox <39821509+Patbox@users.noreply.github.com>
2022-06-11 21:20:18 +0200

    Block map updates

--- src/main/java/com/parzivail/blanketconpatches/BlanketconPatches.java
+++ src/main/java/com/parzivail/blanketconpatches/BlanketconPatches.java
@@ -1,6 +1,9 @@
 package com.parzivail.blanketconpatches;
 
 import net.fabricmc.api.ModInitializer;
+import net.fabricmc.fabric.api.gamerule.v1.GameRuleFactory;
+import net.fabricmc.fabric.api.gamerule.v1.GameRuleRegistry;
+import net.minecraft.world.GameRules;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -9,6 +12,8 @@ public final class BlanketconPatches implements ModInitializer
 	public static final Logger LOGGER = LoggerFactory.getLogger("BlanketCon Patches");
 
 	public static boolean SHOULD_PATCH_KEYS = false;
+
+	public static final GameRules.Key<GameRules.BooleanRule> BLOCK_MAPS = GameRuleRegistry.register("blockMapUpdates", GameRules.Category.MISC, GameRuleFactory.createBooleanRule(false));
 
 	@Override
 	public void onInitialize()
